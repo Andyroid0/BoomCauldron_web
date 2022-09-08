219becf79170872043d1579894c34f3481f71ae4
Andrew <andrewpvalley@gmail.com>
2022-09-08 01:44:12 -0400

    easystar and start of tile data object logic

--- Client/src/Scenes/singleCauldron/Cauldron_start.ts
+++ Client/src/Scenes/singleCauldron/Cauldron_start.ts
@@ -1,5 +1,6 @@
 import Phaser from 'phaser'
 import Player from '../../Actors/Players/Player';
+import Color from 'color';
 
 
 export default class Cauldron_start extends Phaser.Scene
@@ -17,7 +18,7 @@ export default class Cauldron_start extends Phaser.Scene
 	preload() {
         this.load.image('crystal1', 'crystal1.png');
         this.load.image('bush', 'bush_1.png');
-
+        this.load.tilemapImpact
     }
 
     create() {
@@ -25,6 +26,11 @@ export default class Cauldron_start extends Phaser.Scene
         this.player = new Player( this.matter.world, this, 100, 100);
         this.add.image(400, 400, 'crystal1');
         this.add.text(100, 100, 'Cauldron Start');
+
+        this.add.rectangle( 500, 500, 100, 100, Color("#c756c7").rgbNumber().valueOf());
+
+        this.add.tilemap(undefined, 100, 100, 10, 10);
+        
     }
 
     update(time: number, delta: number): void {
@@ -32,6 +38,23 @@ export default class Cauldron_start extends Phaser.Scene
 
     }
 }
+
+
+// Tile should have a custom image object that sets it to the background etc.
+
+type Tile = {
+
+    obstruction: boolean,
+    img: Phaser.GameObjects.Image,
+    localIndex: {x: number, y: number},
+    areaIndex: {x: number, y: number},
+    globalIndex: {x: number, y: number},
+    width: number,
+    height: number
+
+}
+
+
 
 
 
